05c9a2a9312e81c05c3561f8f03534ce914544e2
Mahedi <mahidehasansiyam123@gmail.com>
2026-07-14 20:13:43 +0600

    Initial commit: client app

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Sidebar } from "@/components/Drawer";
+
+
+export default function Home() {
+  return (
+    <div className="">
+      <h2>This is home page of shopor</h2>
+      <Sidebar></Sidebar>
+    </div>
+  );
+}
